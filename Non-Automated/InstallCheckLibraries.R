3b55be52f66dc74da2a1fb9a579d18f855bda690
itwasthatguy <50920368+itwasthatguy@users.noreply.github.com>
2020-05-29 10:17:46 -0400

    No longer install keras

--- Non-Automated/InstallCheckLibraries.R
+++ Non-Automated/InstallCheckLibraries.R
@@ -6,8 +6,6 @@ install.packages('SPEI')
 install.packages('qmap')
 install.packages('sf')
 install.packages('ranger')
-install.packages('tensorflow')
-install.packages('keras')
 
 library(parallel)
 library(doParallel)
@@ -16,12 +14,3 @@ library(ncdf4)
 library(SPEI)
 library(qmap)
 library(sf)
-library(keras)
-
-########  
-########
-#install_tensorflow(version = "gpu")
-#library(ranger)
-#install_keras(tensorflow = "gpu")
-########
-########
